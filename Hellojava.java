9e4ecd099eda54b5f3492a50cf985ac0563fa4f2
Satish Gubbala <Satishkumar82@gmail.com>
2018-07-07 10:51:15 -0400

    hello java

---
+++ Hellojava.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i =1 ;i<=4;i++)
+		{
+		System.out.println("Hello Java"+i);
+		}
+
+	}
+
+}
