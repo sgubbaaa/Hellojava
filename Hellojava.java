de3aaa25a37629450f7fa1723bb924e90dd8f5b0
Satish Gubbala <Satishkumar82@gmail.com>
2018-07-07 11:07:20 -0400

    hello.java

--- Hellojava.java
+++ Hellojava.java
@@ -4,9 +4,9 @@ public class Hellojava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i =1 ;i<=4;i++)
+		for(int i =1 ;i<=5;i++)
 		{
-		System.out.println("Hello Java"+i);
+		System.out.println("Hello Java--->"+i);
 		}
 
 	}
